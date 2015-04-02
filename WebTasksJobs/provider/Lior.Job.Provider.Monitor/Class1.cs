93bcc39315baaf84f3c19bfd15353aef64eccc1f
liorg <lior.grosman@gmail.com>
2015-04-02 14:48:06 +0300

    hh

---
+++ WebTasksJobs/provider/Lior.Job.Provider.Monitor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lior.Job.Provider.Monitor
+{
+    public class Class1
+    {
+    }
+}
