30b8501f958a1d1ba8839febff1b706065f1dea0
liorg <lior.grosman@gmail.com>
2015-04-02 14:30:34 +0300

    ddd

---
+++ WebTasksJobs/services/Site/Lior.Job.Service.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lior.Job.Service.Site.MvcApplication" Language="C#" %>
---
+++ WebTasksJobs/services/Site/Lior.Job.Service.Site/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Lior.Job.Service.Site.Startup))]
+
+namespace Lior.Job.Service.Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
